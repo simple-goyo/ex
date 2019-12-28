fffe8061d65264edbf5c6c9bd70c145c804203e4
shenbiao <1105125966@qq.com>
2019-12-28 16:21:18 +0800

    1

---
+++ src/Common/js/url-constant.js
@@ -0,0 +1,18 @@
+const HostName =  {
+  'ServiceCenter': '192.168.1.116:5000',
+  'ImageRecognize': '192.168.1.116:5000',
+}
+
+const RequestName =  {
+  'GET_VOICE_START' : 'get_voice_start',
+}
+
+// function getServiceCenterUrl(requestName) {
+//   const serviceURL = 'http://' + HostName.ServiceCenter + '/' + requestName;
+//   return serviceURL;
+// }
+
+export default {
+  HostName,
+  RequestName,
+}
--- src/VoiceHome/voice-home.js
+++ src/VoiceHome/voice-home.js
@@ -1,15 +1,2 @@
-import router from '@system.router'
 
-export default {
-  // 页面级组件的数据模型，影响传入数据的覆盖机制：private内定义的属性不允许被覆盖
-  private: {
-    title1: '示例页0'
-  },
-  routeDemo () {
-    // 跳转到应用内的某个页面，router用法详见：文档->接口->页面路由
-    router.push ({
-      uri: '/Demo'
-    })
-  }
-}
   
