77c0076190f9c05961c68eda3ae41d407a6754bf
figo hu <okjjhfj111@126.com>
2019-12-30 16:46:16 +0800

    修改文件

---
+++ src/VoiceRecord/constant.js
@@ -0,0 +1,14 @@
+const BaseUrl = 'http://192.168.1.103:8080/'
+
+const WebAddr = {
+    VoiceUrl:   BaseUrl+'quickapp/voicerecord/voice_record.html',
+    RecoUrl:   BaseUrl+'quickapp/batchSaveVoice',
+    TextUrl:    BaseUrl+'quickapp/housekeeper/canvas-side-text/index.html?a=',
+    ResourceUrl:BaseUrl+'quickapp/sourceCenter/source_center.html',
+    ServiceUrl: BaseUrl+'quickapp/serviceCenter/service_center.html',
+    PhotoUri:   '/camera'
+}
+export {
+    BaseUrl,
+    WebAddr
+}
