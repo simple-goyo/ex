47d65fb8bcb947bfb59f6484390ef06ff4b0339a
shenbiao <1105125966@qq.com>
2019-12-30 22:32:48 +0800

    a

--- src/Common/js/constant.js
+++ src/Common/js/constant.js
@@ -4,10 +4,12 @@ const BaseUrl = 'http://10.141.221.88:8080/sc/'
 const WebAddr = {
     VoiceUrl:   BaseUrl+'quickapp/voicerecord/voice_record.html',
     RecoUrl:   BaseUrl+'quickapp/batchSaveVoice',
+    TextUrl0:    BaseUrl+'quickapp/housekeeper/canvas-side-text/index.html?a=',
     TextUrl:    BaseUrl+'quickapp/housekeeper/canvas-side-text/index.html?a=好的@',
     ResourceUrl:BaseUrl+'quickapp/sourceCenter/source_center.html',
     ServiceUrl: BaseUrl+'quickapp/serviceCenter/service_center.html',
-    PhotoUri:   '/camera'
+    PhotoUri:   '/camera',
+    QueryInfo:BaseUrl+'quickapp/queryInfo'
 }
 export {
     BaseUrl,
